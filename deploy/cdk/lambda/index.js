279ab0a90ac201f583ef518367c9bb04fcd93f8f
sarangan2209 <sarangan@tecofize.com>
2025-07-01 13:35:34 +0530

    update

---
+++ deploy/cdk/lambda/index.js
@@ -0,0 +1,29 @@
+exports.handler = async (event) => {
+  const request = event.Records[0].cf.request;
+  const headers = request.headers;
+
+  const user = 'admin';
+  const pass = 'password';
+  const authString = 'Basic ' + Buffer.from(`${user}:${pass}`).toString('base64');
+
+  if (
+    headers.authorization &&
+    headers.authorization[0].value === authString
+  ) {
+    return request;
+  }
+
+  return {
+    status: '401',
+    statusDescription: 'Unauthorized',
+    headers: {
+      'www-authenticate': [
+        {
+          key: 'WWW-Authenticate',
+          value: 'Basic realm="Restricted Area"',
+        },
+      ],
+    },
+    body: 'Authentication required',
+  };
+};
